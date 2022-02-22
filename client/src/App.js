768abd7f5c9f1c2dece2a9dc54e14b2753ab4e5a
robyng <robyngraham01@gmail.com>
2022-02-22 14:24:57 -0800

    add header, nav, and Landing to App.js, add menu function to App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,40 @@
-
 import './App.css';
 import Login from './components/Login';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Nav from './components/Nav';
+import Landing from './components/Landing';
+import Header from './components/Header';
+import React, { useState } from 'react';
 
 function App() {
+
+  const pages = [
+    "Home",
+    "Login"
+  ]
+
+  const [currentPage, setCurrentPage] = useState(pages[0]);
+  function displayPage() {
+    if (currentPage === "Home") {
+      return <Landing />
+    } else if (currentPage === "Login") {
+      return <Login />
+    }
+  }
   return (
     <div>
-      <main>
-        <Login></Login>
-      </main>
+      <Header>
+      <Nav
+      pages={pages}
+      currentPage={currentPage}
+      setCurrentPage={setCurrentPage}>
+      </Nav>
+
+
+      </Header>
+
+     {displayPage()}
+
     </div>
   );
 }
