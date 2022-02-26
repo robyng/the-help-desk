ccc6743822645b96b92e713b9c4d124ca2cea1cb
mogannam <joemogannam@gmail.com>
2022-02-25 18:59:37 -0800

    env variable partial fix

--- client/src/components/FileDownload/index.js
+++ client/src/components/FileDownload/index.js
@@ -5,11 +5,11 @@ import { uploadFile } from 'aws-sdk';
 
 var AWS = require("aws-sdk");
 const config = {
-    bucketName: "the-help-desk",
-    dirName: 'ticket-number' /* optional */,
-    region: "us-west-1",
-    accessKeyId: "",
-    secretAccessKey: ""
+    bucketName: process.env.REACT_APP_BUCKET_NAME,
+    dirName: process.env.REACT_APP_DIR_NAME /* optional */,
+    region: process.env.REACT_APP_REGION,
+    accessKeyId: process.env.REACT_APP_ACCESS_ID,
+    secretAccessKey: process.env.REACT_APP_ACCESS_KEY
 };
 let img
 
--- client/src/components/FileUpload/index.js
+++ client/src/components/FileUpload/index.js
@@ -1,13 +1,15 @@
 import React, {useRef,useState} from 'react';
 import { uploadFile } from 'aws-sdk';
+//require('dotenv').config()
+
 
 var AWS = require("aws-sdk");
 const config = {
-  bucketName: "the-help-desk",
-  dirName: 'ticket-number' /* optional */,
-  region:"us-west-1",
-  accessKeyId: "",
-  secretAccessKey: ""
+  bucketName: process.env.REACT_APP_BUCKET_NAME,
+  dirName: process.env.REACT_APP_DIR_NAME /* optional */,
+  region: process.env.REACT_APP_REGION,
+  accessKeyId: process.env.REACT_APP_ACCESS_ID,
+  secretAccessKey: process.env.REACT_APP_ACCESS_KEY
 };
 
 
