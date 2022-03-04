437c30c1f189c2c86c0592b76c6bd806f7da76b0
David Barron <90076120+webgeekbear@users.noreply.github.com>
2022-03-04 11:21:14 -0800

    Get login working no menu

--- client/src/App.js
+++ client/src/App.js
@@ -20,84 +20,89 @@ import {
   ApolloProvider,
   createHttpLink,
 } from '@apollo/client';
-
 import { setContext } from '@apollo/client/link/context';
-
-
 const httpLink = createHttpLink({
   uri: '/graphql',
 });
-
 const authLink = setContext((_, { headers }) => {
   const token = localStorage.getItem('id_token');
   return {
     headers: {
       ...headers,
-      authorization: token ? `Bearer ${token}` : '',
+    authorization: token ? `Bearer ${token}` : '',
     },
   };
 });
-
 const client = new ApolloClient({
   link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
-
-/*for log out kill session*/
+//for log out kill session/
 const logout = event => {
   // event.preventDefault();
   Auth.logout();
 };
-
 function App() {
   // const pages = [{title:"Home",link: "/"}, {title:"Login", link: "/login"}, "Signup"];
   // const memberPages = ["NewTicket", "Dashboard", "Logout", "Account"]
-
   // const [currentPage, setCurrentPage] = useState(pages[0]);
-
 function handleActive(e) {
   e.preventDefault()
   e.class = "btn active-page"
 }
-
-  function displayNav() {
-    if (Auth.loggedIn() && Auth.getInfo().unit === '000') {
-      return <>
-        <Link to="/new-ticket" className="btn" name="new-ticket">New Ticket</Link>
-        <Link to="/dashboard" className="btn" name="dashboard">Dashboard</Link>
-        <a href="/" onClick={logout} className="btn" alt='' name="logout">Logout</a>
-        <Link to="/admin-account" className="btn" name="admin-account">Account</Link>
-      </>
-
-    } else if ( Auth.loggedIn() ) {
-      return <>
-        <Link to="/new-ticket" className="btn" name="new-ticket">New Ticket</Link>
-        <Link to="/dashboard" className="btn" name="dashboard" >Dashboard</Link>
-        <a href='/' onClick={logout()} className="btn" name="logout">Logout</a>
-        <Link to="/account" className="btn" name="account">Account</Link>
-        </>
-    } else {
-      return <> 
-        <Link to="/" className="btn" name="home">Home</Link>
-        <Link to="/login" className="btn" name="login">Login</Link>
-        <Link to="/signup" className="btn" name="signup">Sign Up</Link>
-      </>
-    }
+  // function displayNav() {
+  //   if (Auth.loggedIn() && Auth.getInfo().unit === '000') {
+  //     return <>
+  //       <Link to="/new-ticket" className="btn" name="new-ticket">New Ticket</Link>
+  //       <Link to="/dashboard" className="btn" name="dashboard">Dashboard</Link>
+  //       <a href="/" onClick={logout} className="btn" alt='' name="logout">Logout</a>
+  //       <Link to="/admin-account" className="btn" name="admin-account">Account</Link>
+  //     </>
+  //   } else if ( Auth.loggedIn() ) {
+  //     return <>
+  //       <Link to="/new-ticket" className="btn" name="new-ticket">New Ticket</Link>
+  //       <Link to="/dashboard" className="btn" name="dashboard" >Dashboard</Link>
+  //       <a href='/' onClick={logout()} className="btn" name="logout">Logout</a>
+  //       <Link to="/account" className="btn" name="account">Account</Link>
+  //       </>
+  //   } else {
+  //     return <>
+  //       <Link to="/" className="btn" name="home">Home</Link>
+  //       <Link to="/login" className="btn" name="login">Login</Link>
+  //       <Link to="/signup" className="btn" name="signup">Sign Up</Link>
+  //     </>
+  //   }
+  // }
+  const pages = ["Home", "Login", "Signup"];
+  const memberPages = ["NewTicket", "Dashboard", "Logout", "Account"]
+  const [currentPage, setCurrentPage] = useState(pages[0]);
+  function displayPage() {
+    if (currentPage === "Home") {
+      return <Landing />;
+    } else if (currentPage === "Login") {
+      return <Login />;
+    } else if (currentPage === "Signup") {
+      return <Signup />;
+    } else if (currentPage === "NewTicket" && Auth.loggedIn()) {
+        return <NewTicket />;
+    } else if (currentPage === "Dashboard" && Auth.loggedIn()) {
+    return <Dashboard />;
+  }  else if (currentPage === "Account" && Auth.loggedIn()) {
+    return Auth.getInfo().unit === '000'?<AdminAccount/>:<Account />;
+  }else if (currentPage === "Logout") {
+    /*don't forget to use Auth.logout as a function! use Auth.logout() with parentheses */
+    return {logout} && Auth.logout()
+  }else {
+    return <Login></Login>
   }
-  
+}
   return (
     <ApolloProvider client={client}>
       <Router>
         <div>
           <div className="hero">
             <Header>
-
-{displayNav()}
-
-            </Header>
-
-            <div className="content">
-<Switch>
+            <Switch>
               <Route exact path="/" component={Landing} />
               <Route exact path="/login" component={Login} />
               <Route exact path="/signup" component={Signup} />
@@ -105,19 +110,17 @@ function handleActive(e) {
               <Route exact path="/new-ticket" component={NewTicket} />
               <Route exact path="/account" component={Account} />
               <Route exact path="/admin-account" component={AdminAccount} />
-
               <Route component={NoMatch} />
               </Switch>
+            </Header>
+            <div className="content">
+            {displayPage()}
 
             </div>
-
-
           </div>
-
         </div>
       </Router>
     </ApolloProvider>
   );
 }
-
 export default App;
